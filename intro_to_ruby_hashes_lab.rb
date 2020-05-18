852f562505721af505d584beeb3d436a0ab1f2a8
Esther Kang <estherkang14@gmail.com>
2020-05-18 21:47:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,6 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   
  bash_hash = {
   railroads: {
@@ -20,7 +16,7 @@ def monopoly_with_second_tier
     rent_in_dollars: { }, 
     names: { }
   }
-
+ }
 end
 
 def monopoly_with_third_tier
